2237139177392b43a3aa68b541e62f11beb4663b
Anzx <0.love.2d.atsi.2@gmail.com>
2020-04-28 02:02:58 +0300

    ♥

--- tbot.py
+++ tbot.py
@@ -135,17 +135,15 @@ def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-
 @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://la-things-bot.herokuapp.com/' + cf.token)
     return "!", 200
 
-
 if __name__ == "__main__":
-    #server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
     pass
 
-bot.remove_webhook()
-bot.polling()
+#bot.remove_webhook()
+#bot.polling()
